97feb9ff675c8b9eb3ee5f4b755cc84b1f883cb9
FaroukBel <fbelkhyate@gmail.com>
2024-04-20 14:56:03 +0100

    Update

---
+++ src/main/java/com/gdr/gdrpfa/GdrPfaApplication.java
@@ -0,0 +1,13 @@
+package com.gdr.gdrpfa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GdrPfaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GdrPfaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gdr/gdrpfa/GdrPfaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gdr.gdrpfa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GdrPfaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
